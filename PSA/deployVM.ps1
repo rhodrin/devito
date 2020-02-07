335bbf414cb83a0d71da8a677a3b7da26cf26380
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-02-07 13:37:01 +0000

    Test.

--- PSA/deployVM.ps1
+++ PSA/deployVM.ps1
@@ -81,7 +81,7 @@ try {
     az vm create  `
         --resource-group $resourceGroupName `
         --name $serverName `
-        --image $image `
+        --image gpuImage `
         --admin-username $adminLogin `
         --admin-password $adminPassword
     }
